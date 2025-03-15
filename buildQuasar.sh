7a101d39a215e522804820d536b7ea9d423bf91b
hunter <18358821@qq.com>
2025-03-15 18:02:18 +0800

    1

--- buildQuasar.sh
+++ buildQuasar.sh
@@ -1,63 +1,110 @@
 #!/bin/bash
+
+# 日志输出函数
+log() {
+    local level=$1
+    local message=$2
+    printf "[%s] %s\n" "$level" "$message"
+}
+
+# 验证输入参数是否为有效的数字
+if ! [[ $1 =~ ^[0-4]$ ]]; then
+    log "ERROR" "输入参数无效，请输入 0 到 4 之间的数字。"
+    exit 1
+fi
+
 levelKey=${1}
-printf '执行参数：levelKey[{%s}] \n' "$levelKey"
-levelValue=''
-if [ ! "$levelKey" ]; then
-    levelKey=2
-    levelValue='vueGo'
-elif [ $levelKey == 0 ]; then
-    levelKey=0
-    levelValue='none'
-elif [ $levelKey == 1 ]; then
-    levelValue='vue'
-elif [ $levelKey == 2 ]; then
-    levelValue='vueGo'
-fi
-printf '执行级别：levelKey[{%s}]--levelValue[{%s}] \n' $levelKey $levelValue
-echo 'cd ./qapp'
-cd ./qapp
-echo 'rm -rf dist'
-rm -rf dist
-# echo 'mkdir -p dist'
-# mkdir -p dist
-echo 'cd ..'
-cd ..
+log "INFO" "执行参数：levelKey[${levelKey}]"
 
-if [ $levelKey == 1 ] || [  $levelKey == 2 ] || [ $levelKey == 3 ] || [ $levelKey == 4 ]; then
-    echo 'cd electron_quasar'
-    cd electron_quasar
-    echo 开始打包前端文件
-    echo 'yarn build'
-    yarn build
-    echo '打包完成，开始移动到app目录'
-    echo 'cp -R dist/spa ../qapp/dist'
-    # cp -R dist/spa ../qapp/dist
-    # mv dist/spa ../qapp
-    cp -R dist/spa ../qapp/dist
-fi
-cd ..
-if [ $levelKey == 2 ] || [ $levelKey == 4 ]; then
-    echo '移动完成，打包APP'
-    cd gosrc
-    go build -o ../qapp/appQuaser.exe -ldflags "-H=windowsgui" -tags=prod
-    # go build -o ../qapp/appQuaser.exe  -tags=prod
-    echo 移动配置文件 '*.*(1)'
-    echo 'cp setting.json ../qapp/setting.json(1)'
-    cp setting.json '../qapp/setting.json(1)'
-    echo 'cp ffmpeg.exe ../qapp/ffmpeg.exe'
-    cp ffmpeg.exe '../qapp/ffmpeg.exe'
-    cd ..
-    echo 'APP打包完成！！！'
-fi
+# 根据 levelKey 设置 levelValue
+get_level_value() {
+    case $1 in
+        0)
+            echo "none"
+            ;;
+        1)
+            echo "vue"
+            ;;
+        2)
+            echo "vueGo"
+            ;;
+        *)
+            echo ""
+            ;;
+    esac
+}
 
-if  [ $levelKey == 3 ] || [ $levelKey == 4 ]; then
-    echo 移动源到 Election代码目录
-    echo 'cp -R qapp electron_quasar/src-electron/icons'
-    cp -R qapp electron_quasar/src-electron/icons
-    echo 'cd electron_quasar'
-    cd electron_quasar
-    yarn topc
-    echo 'Electron Package OVER'
-#TODO
-fi
-echo 'SUCCESS,OVER !!!'
+levelValue=$(get_level_value "$levelKey")
+log "INFO" "执行级别：levelKey[${levelKey}]--levelValue[${levelValue}]"
+
+# 清理 qapp 目录
+clean_qapp() {
+    log "INFO" "清理 qapp 目录..."
+    cd ./qapp || { log "ERROR" "无法进入 qapp 目录"; exit 1; }
+    rm -rf dist
+    cd .. || { log "ERROR" "无法返回上级目录"; exit 1; }
+}
+
+# 打包前端文件
+build_frontend() {
+    log "INFO" "开始打包前端文件..."
+    cd electron_quasar || { log "ERROR" "无法进入 electron_quasar 目录"; exit 1; }
+    if ! yarn build; then
+        log "ERROR" "前端文件打包失败"
+        exit 1
+    fi
+    log "INFO" "打包完成，开始移动到 app 目录"
+    cp -R dist/spa ../qapp/dist || { log "ERROR" "移动前端文件失败"; exit 1; }
+    cd .. || { log "ERROR" "无法返回上级目录"; exit 1; }
+}
+
+# 打包 APP
+build_app() {
+    log "INFO" "移动完成，打包 APP..."
+    cd gosrc || { log "ERROR" "无法进入 gosrc 目录"; exit 1; }
+    if ! go build -o ../qapp/appQuaser.exe -ldflags "-H=windowsgui" -tags=prod; then
+        log "ERROR" "APP 打包失败"
+        exit 1
+    fi
+    log "INFO" "移动配置文件 '*.*(1)'"
+    cp setting.json '../qapp/setting.json(1)' || { log "ERROR" "移动 setting.json 文件失败"; exit 1; }
+    cp ffmpeg.exe '../qapp/ffmpeg.exe' || { log "ERROR" "移动 ffmpeg.exe 文件失败"; exit 1; }
+    cd .. || { log "ERROR" "无法返回上级目录"; exit 1; }
+    log "INFO" "APP 打包完成！！！"
+}
+
+# 移动源到 Election 代码目录
+move_to_electron() {
+    log "INFO" "移动源到 Election 代码目录"
+    cp -R qapp electron_quasar/src-electron/icons || { log "ERROR" "移动源文件到 Election 代码目录失败"; exit 1; }
+    cd electron_quasar || { log "ERROR" "无法进入 electron_quasar 目录"; exit 1; }
+    if ! yarn topc; then
+        log "ERROR" "Electron 打包失败"
+        exit 1
+    fi
+    log "INFO" "Electron Package OVER"
+}
+
+# 执行清理操作
+clean_qapp
+
+# 根据 levelKey 执行相应的操作
+case $levelKey in
+    1|2|3|4)
+        build_frontend
+        ;;
+esac
+
+case $levelKey in
+    2|4)
+        build_app
+        ;;
+esac
+
+case $levelKey in
+    3|4)
+        move_to_electron
+        ;;
+esac
+
+log "INFO" "SUCCESS,OVER !!!"
