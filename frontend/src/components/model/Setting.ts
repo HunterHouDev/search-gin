d4ad7df6ade3e571dbb52b14e88e049414364a14
hhd <183598821@qq.com>
2026-06-10 01:35:01 +0800

    feat(setting): 添加时间扫描及播放器等设置项

--- frontend/src/components/model/Setting.ts
+++ frontend/src/components/model/Setting.ts
@@ -8,18 +8,26 @@ export class SettingInfo {
   Types: string[] = ['mp4', 'jpg'];
   Buttons: string[] = ['刮图', '删除'];
   MovieTypes: string[] = ['骑兵', '步兵', '国产', '漫动'];
-  BaseDir: string[] = [];
-  tagLibData: string[] = [];
   TagsLib: string[] = [];
-  Remark: string | undefined;
-  SystemHtml: string | undefined;
+  Pages: string[] = [];
+
+  EnableTimeScan = true;
+  CutThenDelete = false;
+  SystemPlayer = 'ffplay';
+  SystemPlayerVolumn = '30';
+  SystemPlayerWidth = '1280';
+  HardwareAcceleration = false;
+  HardwareAccelMode = '';
+
+  IsDb = false;
+  IsJavBus = false;
+
+  ControllerHost: string | undefined;
   BaseUrl: string | undefined;
   OMUrl: string | undefined;
   ImageUrl: string | undefined;
-  ControllerHost: string | undefined;
-  IsDb = false;
-  HardwareAcceleration = false;
-  HardwareAccelMode = '';
+  Remark: string | undefined;
+  SystemHtml: string | undefined;
 }
 
 export const buttonEnum = [
